ea954fb8f0bc1c1179c30e102036bdb08d13965e
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-08-09 19:44:32 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -19,16 +19,17 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const getSkuFromProductItem = (item) =>
+  item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = (event) => {
-  // coloque seu código aqui
-};
+const cartItemClickListener = (event) => {};
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
@@ -38,17 +39,35 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-const results = async () => {
+const cartItem = async (id) => {
+  const object = await fetchItem(id);
+  const { id: sku, title: name, price: salePrice } = object;
+  const result = { sku, name, salePrice };
+  const cartElement = createCartItemElement(result);
+  const myCart = document.querySelector('.cart__items');
+  myCart.appendChild(cartElement);
+};
+
+const button = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((element) => {
+    element.addEventListener('click', () =>
+      cartItem(element.parentElement.firstChild.innerText));
+  });
+};
+
+const ProductItem = async () => {
   const object = await fetchProducts('computador');
   const products = document.getElementsByClassName('items');
   const objectResults = object.results;
-  objectResults.forEach((element) => { 
+  objectResults.forEach((element) => {
     const { id: sku, title: name, thumbnail: image } = element;
     const result = createProductItemElement({ sku, name, image });
     products[0].appendChild(result);
   });
-  };
+  button();
+};
 
-window.onload = () => { 
-  results();
+window.onload = () => {
+  ProductItem();
 };
