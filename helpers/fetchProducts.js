a5424ce3af7257219cdb6a53ab4e1ea6fd298050
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-08-09 16:09:10 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,4 +1,8 @@
-const fetchProducts = () => {
+const fetchProducts = (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  return fetch(url)
+  .then(resposta => resposta.json())
+  .then(data => data);
   // seu código aqui
 };
 
